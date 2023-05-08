91f574b820182649087d73223dfa064d4501cd90
BruceGliff <ilyaawaA@yandex.ru>
2023-05-08 23:35:53 +0300

    CMake introduced

--- MichaelScottQueue/src/main.cc
+++ MichaelScottQueue/src/main.cc
@@ -1,17 +1,15 @@
 #include "MichaelScottQueue.hpp"
 
+#include <cstdlib>
 #include <thread>
 #include <vector>
 
 std::atomic<int> MsgCount{0};
 std::atomic<int> MsgRead{0};
-constexpr int Msgs = 500;
-constexpr int Readers = 2;
-constexpr int Writers = 3;
 
 using QType = MSQueue<int>;
 
-void read(QType &Q) {
+void read(QType &Q, int Msgs) {
   while (MsgRead < Msgs) {
     if (auto x = Q.dequeue()) {
       ++MsgRead;
@@ -19,7 +17,7 @@ void read(QType &Q) {
   }
 }
 
-void write(QType &Q) {
+void write(QType &Q, int Msgs) {
   while (MsgCount < Msgs) {
     auto val = MsgCount.load();
     if (MsgCount.compare_exchange_strong(val, val + 1)) {
@@ -28,7 +26,23 @@ void write(QType &Q) {
   }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+
+  // Default Msgs value
+  int Msgs = 100;
+  int Readers = 3;
+  int Writers = 2;
+  if (argc >= 2)
+    Msgs = strtol(argv[1], nullptr, 10);
+  if (argc >= 3)
+    Readers = strtol(argv[2], nullptr, 10);
+  if (argc >= 4)
+    Writers = strtol(argv[3], nullptr, 10);
+
+  if (!(Msgs > 0 && Readers > 0 && Writers > 0)) {
+    std::cerr << "Wrong args\n";
+    return -1;
+  }
 
   int ReadersPl = 0;
   int WritersPl = 0;
@@ -41,12 +55,12 @@ int main() {
       if (i % 2) {
         if (ReadersPl < Readers) {
           ReadersPl++;
-          pool.emplace_back(read, std::ref(Q));
+          pool.emplace_back(read, std::ref(Q), Msgs);
         }
       } else {
         if (WritersPl < Writers) {
           WritersPl++;
-          pool.emplace_back(write, std::ref(Q));
+          pool.emplace_back(write, std::ref(Q), Msgs);
         }
       }
       ++i;
